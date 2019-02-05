dc9e2cc79eb38aa8362af3403b8ce633d2354771
BobFrapples <bobfrapples78@hotmail.com>
2019-02-04 19:37:02 -0600

    convert to imagebase

--- TeaseAI.Services/CommandProcessor/ShowBoobsImageCommandProcessor.cs
+++ TeaseAI.Services/CommandProcessor/ShowBoobsImageCommandProcessor.cs
@@ -1,44 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using TeaseAI.Common;
-using TeaseAI.Common.Constants;
-using TeaseAI.Common.Data;
-using TeaseAI.Common.Events;
-using TeaseAI.Common.Interfaces;
+﻿using TeaseAI.Common.Constants;
 using TeaseAI.Common.Interfaces.Accessors;
 
 namespace TeaseAI.Services.CommandProcessor
 {
-    public class ShowBoobsImageCommandProcessor : ICommandProcessor
+    public class ShowBoobsImageCommandProcessor : ShowImageCommandProcessorBase
     {
-        public ShowBoobsImageCommandProcessor(IImageAccessor imageAccessor)
+        public ShowBoobsImageCommandProcessor(IImageAccessor imageAccessor) : base(imageAccessor)
         {
-            _imageAccessor = imageAccessor;
         }
 
-        public event EventHandler<CommandProcessedEventArgs> CommandProcessed;
+        protected override ImageGenre Genre => ImageGenre.Boobs;
 
-        public string DeleteCommandFrom(string line)
-        {
-            return line.Replace(Keyword.ShowBoobImage, string.Empty).Replace(Keyword.ShowBoobsImage, string.Empty);
-        }
-
-        public bool IsRelevant(Session session, string line) => line.Contains(Keyword.ShowBoobImage) || line.Contains(Keyword.ShowBoobsImage);
-
-        public Result<Session> PerformCommand(Session session, string line)
-        {
-            return _imageAccessor.GetImageMetaDataList(default(ImageSource?), ImageGenre.Boobs)
-                .Ensure(data => data.Count > 0, "No  images of genre " + ImageGenre.Boobs.ToString() + " to load")
-                .OnSuccess(data => data[new Random().Next(data.Count)])
-                .OnSuccess(img => OnCommandProcessed(session, img))
-                .Map(img => session);
-        }
-
-        private void OnCommandProcessed(Session session, ImageMetaData selected)
-        {
-            CommandProcessed?.Invoke(this, new CommandProcessedEventArgs() { Session = session, Parameter = selected });
-        }
-
-        private readonly IImageAccessor _imageAccessor;
+        protected override string Keyword => Common.Constants.Keyword.ShowBoobImage;
     }
 }
--- TeaseAI.Services/CommandProcessor/ShowButtImageCommandProcessor.cs
+++ TeaseAI.Services/CommandProcessor/ShowButtImageCommandProcessor.cs
@@ -1,44 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using TeaseAI.Common;
-using TeaseAI.Common.Constants;
-using TeaseAI.Common.Data;
-using TeaseAI.Common.Events;
-using TeaseAI.Common.Interfaces;
+﻿using TeaseAI.Common.Constants;
 using TeaseAI.Common.Interfaces.Accessors;
 
 namespace TeaseAI.Services.CommandProcessor
 {
-    public class ShowButtImageCommandProcessor : ICommandProcessor
+    public class ShowButtImageCommandProcessor : ShowImageCommandProcessorBase
     {
-        public ShowButtImageCommandProcessor(IImageAccessor imageAccessor)
+        public ShowButtImageCommandProcessor(IImageAccessor imageAccessor) : base(imageAccessor)
         {
-            _imageAccessor = imageAccessor;
         }
 
-        public event EventHandler<CommandProcessedEventArgs> CommandProcessed;
+        protected override ImageGenre Genre => ImageGenre.Butt;
 
-        public string DeleteCommandFrom(string line)
-        {
-            return line.Replace(Keyword.ShowButtImage, string.Empty).Replace(Keyword.ShowButtsImage, string.Empty);
-        }
-
-        public bool IsRelevant(Session session, string line) => line.Contains(Keyword.ShowButtImage) || line.Contains(Keyword.ShowButtsImage);
-
-        public Result<Session> PerformCommand(Session session, string line)
-        {
-            return _imageAccessor.GetImageMetaDataList(default(ImageSource?), ImageGenre.Butt)
-                .Ensure(data => data.Count > 0, "No  images of genre " + ImageGenre.Boobs.ToString() + " to load")
-                .OnSuccess(data => data[new Random().Next(data.Count)])
-                .OnSuccess(img => OnCommandProcessed(session, img))
-                .Map(img => session);
-        }
-
-        private void OnCommandProcessed(Session session, ImageMetaData selected)
-        {
-            CommandProcessed?.Invoke(this, new CommandProcessedEventArgs() { Session = session, Parameter = selected });
-        }
-
-        private readonly IImageAccessor _imageAccessor;
+        protected override string Keyword => Common.Constants.Keyword.ShowButtImage;
     }
 }
