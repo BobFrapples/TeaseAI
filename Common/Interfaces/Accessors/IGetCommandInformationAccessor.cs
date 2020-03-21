556a929adeced30867d688c43d3ed7f868fb3541
BobFrapples <bobfrapples78@hotmail.com>
2020-03-21 13:47:32 -0500

    Add command reference. needs data.

---
+++ Common/Interfaces/Accessors/IGetCommandInformationAccessor.cs
@@ -0,0 +1,12 @@
+﻿using TeaseAI.Common.Data;
+
+namespace TeaseAI.Common.Interfaces.Accessors
+{
+    /// <summary>
+    /// Get informatoin about commands
+    /// </summary>
+    public interface IGetCommandInformationAccessor
+    {
+        Result<ScriptCommandInformation> GetCommandInformation(string command);
+    }
+}
